d659cac394c59bb2b6e18f0aab2e7d9cb3bbc1d6
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-09-17 20:15:36 +0600

    35-6 Setting Up Google OAuth Library In The Project

---
+++ src/app/lib/googleAuth.ts
@@ -0,0 +1,8 @@
+import { OAuth2Client } from "google-auth-library";
+import config from "../config";
+
+const googleClient = new OAuth2Client({
+  client_id: config.google_client_id,
+});
+
+export { googleClient };
--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -8,9 +8,10 @@ const router = Router();
 router.post("/register", AuthController.registerPatient);
 router.post("/login", AuthController.loginUser);
 router.get(
-	"/me",
-	auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
-	AuthController.getMe,
+  "/me",
+  auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
+  AuthController.getMe,
 );
 router.post("/refresh-token", AuthController.refreshToken);
+router.post("/google", AuthController.googleLogin);
 export const AuthRoutes = router;
