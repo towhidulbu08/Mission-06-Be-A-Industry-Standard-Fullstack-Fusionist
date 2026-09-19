3b897f99560f5dd0de18983ebe989a037e46d536
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-09-19 22:14:41 +0600

    35-10 Module Summary

--- src/app/lib/googleAuth.ts
+++ src/app/lib/googleAuth.ts
@@ -2,7 +2,7 @@ import { OAuth2Client } from "google-auth-library";
 import config from "../config";
 
 const googleClient = new OAuth2Client({
-  client_id: config.google_client_id,
+	client_id: config.google_client_id,
 });
 
 export { googleClient };
--- src/app/module/auth/auth.interface.ts
+++ src/app/module/auth/auth.interface.ts
@@ -1,23 +1,23 @@
 import type { Role } from "../../../generated/prisma/browser";
 
 export interface ILoginUserPayload {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 export interface IRegisterPatientPayload {
-  name: string;
-  email: string;
-  password: string;
+	name: string;
+	email: string;
+	password: string;
 }
 
 export interface IRequestUser {
-  userId: string;
-  email: string;
-  name: string;
-  role: Role;
+	userId: string;
+	email: string;
+	name: string;
+	role: Role;
 }
 
 export interface IGooleLoginPayload {
-  idToken: string;
+	idToken: string;
 }
